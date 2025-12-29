9c3a05a89ffaa59c9cd65947a0b9603fffabd3c3
Nathanial Hartman <nathanial.hartman@gmail.com>
2025-12-28 22:23:07 -0800

    Switch to GitHub dependencies for v0.0.1 release

--- lakefile.lean
+++ lakefile.lean
@@ -5,7 +5,7 @@ package canopy where
   version := v!"0.1.0"
 
 -- Core dependencies
-require arbor from ".." / "arbor"
+require arbor from git "https://github.com/nathanial/arbor" @ "v0.0.1"
 
 @[default_target]
 lean_lib Canopy where
